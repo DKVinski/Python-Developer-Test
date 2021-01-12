c46647dfc1415c0fbe0e3d09b504fb9219223aa6
dori <dv49899@fer.hr>
2021-01-12 09:18:31 +0100

    sloution

--- Task 3/Task3.py
+++ Task 3/Task3.py
@@ -1,26 +1,28 @@
 import functools
 import time
+from random import random
+from random import seed
+import null
+from self import self
 
 
 def decorator(func):
     @functools.wraps(func)
-    def wrapper_decorator(*args, **kwargs):
+    def wrapper_decorator(cache_key: int = 0, *args, **kwargs):
+        wrapper_decorator.cache = []
         wrapper_decorator.num_calls += 1
-
-        def wrapper_cache(cache_key=None):
-            toc = time.perf_counter()
-            if wrapper_decorator.num_calls == 0 or wrapper_decorator.num_calls > 10:
-                tic = time.perf_counter()
-                if tic - toc < 5 * 60:
-                    wrapper_decorator.num_calls = 0
-                    cache_key = args + tuple(kwargs.items())
-                    if cache_key not in wrapper_cache.cache:
-                        wrapper_cache.cache[cache_key] = func(*args, **kwargs)
-            else:
-                return wrapper_cache.cache[cache_key]
-
-        wrapper_cache.cache = dict()
-        return wrapper_cache
+        toc = time.perf_counter()
+        if wrapper_decorator.num_calls == 1 or wrapper_decorator.num_calls > 10:
+            tic = time.perf_counter()
+            if tic - toc < 5 * 60:
+                wrapper_decorator.num_calls = 0
+                seed((time.perf_counter()-time.perf_counter())*1000)
+                cache_key += int(random())
+                if cache_key not in wrapper_decorator.cache:
+                    wrapper_decorator.cache[cache_key] = func(*args, **kwargs)
+        else:
+            return wrapper_decorator.cache[cache_key]
 
     wrapper_decorator.num_calls = 0
     return wrapper_decorator
+
